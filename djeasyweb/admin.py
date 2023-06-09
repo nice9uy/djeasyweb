e08f0b184b792f7f2de67ad7a7cac9c7f69ed984
Abdul Rachman <abdulrachman.mails@gmail.com>
2023-06-09 19:26:04 +0700

    On progress for build editor page

--- djeasyweb/admin.py
+++ djeasyweb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = "DjEasyweb Admin"
+admin.site.site_title = "UMSRA Admin Portal"
+admin.site.index_title = "Welcome to DjEasyweb"
--- djeasyweb/views.py
+++ djeasyweb/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
+
+def check_admin(user):
+   return user.is_superuser
+
+@user_passes_test(check_admin)
 def home(request):
     return render(request, 'index.html')
